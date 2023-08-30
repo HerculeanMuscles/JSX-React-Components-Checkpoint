e576c892da85c11b5eea10d8d29fe1fd7f0569da
dobbysock69 <103392445+dobbysock69@users.noreply.github.com>
2023-08-30 17:25:35 +0100

    created product.js, and other js files

---
+++ src/components/product.js
@@ -0,0 +1,8 @@
+const product = {
+	name: "Product Name",
+	price: 10.99,
+	description: "This is a sample product description.",
+	image: "https://example.com/product-image.jpg",
+};
+
+export default product;
