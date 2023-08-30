f79d7fbf90552070bd884efe456c5a074e0e771a
dobbysock69 <103392445+dobbysock69@users.noreply.github.com>
2023-08-30 17:54:03 +0100

    added the components into a card

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import "./App.css";
 import Description from "./components/Description";
 import Image from "./components/ImagePath";
 import Price from "./components/Price";
+import Card from "react-bootstrap/Card";
+import product from "./components/product";
 
 function App() {
 	return (
 		<>
 			<h1>Hello</h1>
-			<Name />
+
 			<Description />
-			<Image />
+
 			<Price />
+
+			<Card style={{ width: "18rem" }}>
+				<Card.Img variant="top" src={product.image} />
+				<Card.Body>
+					<Card.Title>
+						<Name />
+					</Card.Title>
+					<Card.Text>
+						<Description />
+						<Price />
+					</Card.Text>
+				</Card.Body>
+			</Card>
 		</>
 	);
 }
